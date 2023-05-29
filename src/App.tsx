9e7d6ef182c765a8f0289c6366bdc1e653035cad
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-05-29 16:38:00 +0530

    Adding React FAB sample

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css';
+import {ListViewComponent} from '@syncfusion/ej2-react-lists';
+import {FabComponent} from '@syncfusion/ej2-react-buttons'
+function App() {
+    let listObj: ListViewComponent;
+    const listData = [
+      { id: 's_01', text: 'Robert' },
+      { id: 's_02', text: 'Nancy' },
+      { id: 's_05', text: 'Jenifer' },
+      { id: 's_03', text: 'Andrew' },
+      { id: 's_06', text: 'Margaret' },
+      { id: 's_07', text: 'Steven' },
+      { id: 's_08', text: 'Michael' }
+    ];
+    const onBtnClk=()=>{
+      let newItem = [{id:'s_09', text: 'Johnson'}];
+      listObj.addItem(newItem)
+    }
+    return (
+    <div className='App'>
+     <ListViewComponent ref={((list:ListViewComponent)=>listObj=list)}
+      id='list' 
+      dataSource={listData}
+      showHeader={true} 
+      headerTitle='Contacts'
+      width={300}></ListViewComponent>
+
+      <FabComponent target='#list' 
+      iconCss='e-icons e-plus e-small' 
+      onClick={onBtnClk}
+      iconPosition='Left'
+      position='BottomRight'
+      cssClass='e-outline'>Add</FabComponent>
+    </div>
+  );
+}
+
+export default App;
